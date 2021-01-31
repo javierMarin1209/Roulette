2ac3736b209211dbc41f0c465af2ccb7e66b8349
JavierMarin97 <41657658+JavierMarin97@users.noreply.github.com>
2021-01-30 19:49:04 -0500

    delet useless imports

--- src/main/java/com/masivian/roulette/controller/WsRoulette.java
+++ src/main/java/com/masivian/roulette/controller/WsRoulette.java
@@ -5,13 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.masivian.roulette.object.RequestCreateBet;
-import com.masivian.roulette.object.ResponseCreateBet;
 import com.masivian.roulette.object.ResponseCreateRoulette;
 import com.masivian.roulette.object.ResponseListRoulettes;
 import com.masivian.roulette.object.ResponseOpenRoulette;
